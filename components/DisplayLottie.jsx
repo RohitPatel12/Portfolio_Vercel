c7f8e09d6b8c77f606293b48b1cf6940ee1216df
Rohit <itsrohitpatel45@gmail.com>
2025-01-29 05:04:04 +0530

    Hosting related fixes

--- components/DisplayLottie.jsx
+++ components/DisplayLottie.jsx
@@ -1,18 +1,40 @@
+// import React from "react";
+// import Lottie from "react-lottie";
+
+// const GreetingLottie = ({ animationPath }) => {
+// 	const defaultOptions = {
+// 		loop: true,
+// 		autoplay: true,
+// 		path: animationPath,
+// 	};
+  
+// 	return (
+// 		<div onClick={() => null}>
+// 			<Lottie options={defaultOptions} />
+// 		</div>
+// 	);
+// };
+
+// export default GreetingLottie;
+
 import React from "react";
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie with SSR disabled
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 const GreetingLottie = ({ animationPath }) => {
-	const defaultOptions = {
-		loop: true,
-		autoplay: true,
-		path: animationPath,
-	};
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    path: animationPath,
+  };
 
-	return (
-		<div onClick={() => null}>
-			<Lottie options={defaultOptions} />
-		</div>
-	);
+  return (
+    <div onClick={() => null}>
+      <Lottie options={defaultOptions} />
+    </div>
+  );
 };
 
 export default GreetingLottie;
